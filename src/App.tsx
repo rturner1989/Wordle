92866c3ef39bd74b87c3f66a54472c35ce31dbbf
rturner1989 <robturner1000@hotmail.co.uk>
2022-05-01 15:59:32 +0100

    Created function for single word selection
    - based on game mode selection, word will update for the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { singleWord } from "./Library/Interface";
 import GameSelection from "./Components/GameSelection";
 
 function App() {
     const [wordData, setWordData] = useState<singleWord[] | null>(null);
+    const [gameWord, setGameWord] = useState<singleWord | null>(null);
+
+    const gameWordSelection = () => {
+        if (!wordData) return;
+        const random = Math.floor(Math.random() * wordData.length);
+        setGameWord(wordData[random]);
+    };
+
+    useEffect(() => {
+        gameWordSelection();
+    }, [wordData]);
 
     return (
         <div className="App">
             {!wordData ? (
                 <GameSelection modeSelect={setWordData} />
             ) : (
-                <div>
-                    {wordData.map((item, index) => {
-                        const { word } = item;
-                        return <p key={index}>{word}</p>;
-                    })}
-                </div>
+                <div>{gameWord?.word}</div>
             )}
         </div>
     );
